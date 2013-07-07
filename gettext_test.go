254cc336620ce07bdfd55e78f0f6bbb2e7c00866
Christian Neumann <cneumann@datenkarussell.de>
2013-07-07 11:47:58 +0200

    Update copyright notice.

--- gettext_test.go
+++ gettext_test.go
@@ -1,18 +1,18 @@
-// This file is part of monsti/util.
-// Copyright 2012-2013 Christian Neumann
+// This file is part of monsti/gettext.
+// Copyright 2013 Christian Neumann
 
-// monsti/util is free software: you can redistribute it and/or modify it under
+// monsti/gettext is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
 // Software Foundation, either version 3 of the License, or (at your option) any
 // later version.
 
-// monsti/util is distributed in the hope that it will be useful, but WITHOUT
+// monsti/gettext is distributed in the hope that it will be useful, but WITHOUT
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 // FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
 // details.
 
 // You should have received a copy of the GNU Lesser General Public License
-// along with monsti/util. If not, see <http://www.gnu.org/licenses/>.
+// along with monsti/gettext. If not, see <http://www.gnu.org/licenses/>.
 
 package gettext
 
